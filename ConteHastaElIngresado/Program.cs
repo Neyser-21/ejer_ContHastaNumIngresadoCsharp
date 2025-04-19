0d615bb7ea134de1b14b8ffdf4363d85bff0b890
Neyser-21 <neyserrivas82@gmail.com>
2025-04-19 16:27:04 -0600

    solicite la carga de un valor positivo y nos muestre desde 1 hasta el valor ingresado

---
+++ ConteHastaElIngresado/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConteHastaElIngresado
+{
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numeroUser;
+           
+            
+            Console.WriteLine("Ingres el numero al que desea llegar ");
+            numeroUser = int.Parse(Console.ReadLine());
+
+
+            BucleContador(numeroUser);
+        }
+
+        private static void BucleContador(int numeroUser)
+        {
+            int contador = 1;
+            while (contador <= numeroUser)
+            {
+                Console.WriteLine("El número es: {0}", contador);
+                contador++;
+
+            }
+            
+        }
+    }
+}
