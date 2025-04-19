4ae36d68592b1b2121e7e1eb7aec39549cfbdac2
Neyser-21 <neyserrivas82@gmail.com>
2025-04-19 16:58:18 -0600

    agrege la verificacion de si es mayor o menor a cero

--- ConteHastaElIngresado/Program.cs
+++ ConteHastaElIngresado/Program.cs
@@ -7,16 +7,44 @@ namespace ConteHastaElIngresado
     {
         public static void Main(string[] args)
         {
-            int numeroUser;
+            int numeroUser = 0;
+            string verificacion;
+
+            //blucle que solicita el numero mientras sea 0 o menos
+            while (numeroUser <= 0)
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese el numero al que desea llegar ");
+                    numeroUser = int.Parse(Console.ReadLine());
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("¡Error! Ingrese un número valido.");
+                }
+                
+                //variable y mtdo que verifica si es > 0
+                verificacion = VerificacionPositivo(numeroUser);
+                Console.WriteLine(verificacion);
+                
+            }
            
-            
-            Console.WriteLine("Ingres el numero al que desea llegar ");
-            numeroUser = int.Parse(Console.ReadLine());
-
-
+            //llamada el contador
             BucleContador(numeroUser);
+            
+            
+            Console.WriteLine("\nPrograma Finalizado. Presiona una tecla para salir...");
+            Console.ReadKey();
         }
-
+        
+        //mtdo que verifica si es mayor a cero
+        static string VerificacionPositivo(int numeroUser)
+        {
+            return (numeroUser <= 0) ? "Asegurese que el numero sea positivo mayor a cero\n" : "\n";
+        }
+        
+        //mtdo bucle que incrementa hasta el numero del usuario
         private static void BucleContador(int numeroUser)
         {
             int contador = 1;
@@ -26,6 +54,7 @@ namespace ConteHastaElIngresado
                 contador++;
 
             }
+            
             
         }
     }
